b166c887af824df187c64e6cf9148d2045ce91ab
恩宇 <e.enyuu812@gmail.com>
2020-06-17 04:53:16 +0800

    to wish

---
+++ app/src/main/java/com/example/ice/Views/WishView.java
@@ -0,0 +1,64 @@
+package com.example.ice.Views;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+import com.example.ice.R;
+
+public class WishView extends View{
+
+    public WishView(Context context){
+        super(context);
+
+        init(null);
+    }
+
+    public WishView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+
+        init(attrs);
+    }
+
+    public WishView(Context context, AttributeSet attrs, int defStyleAttrs){
+        super(context, attrs, defStyleAttrs);
+
+        init(attrs);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public WishView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes){
+        super(context, attrs, defStyleAttr, defStyleRes);
+
+        init(attrs);
+    }
+
+    private void init(@Nullable AttributeSet set){
+
+    }
+
+    @SuppressLint("DrawAllocation")
+    protected void onDraw(Canvas canvas){
+
+
+        Paint paint;
+        paint= new Paint();
+
+        //wish pool
+        @SuppressLint("DrawAllocation") Bitmap wish_pool= BitmapFactory.decodeResource(getResources(), R.drawable.wish_pool);
+        wish_pool = Bitmap.createScaledBitmap(wish_pool, getWidth(), getHeight()-200, true);
+        canvas.drawBitmap(wish_pool,0, 0, paint);
+
+    }
+
+}
--- app/src/main/java/com/example/ice/WishActivity.java
+++ app/src/main/java/com/example/ice/WishActivity.java
@@ -1,13 +1,11 @@
 package com.example.ice;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.util.AttributeSet;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -15,6 +13,7 @@ import com.example.ice.Views.ChildView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -23,22 +22,20 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class MainChildActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class WishActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_child);
+        setContentView(R.layout.activity_wish);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
-
-        final float[] angle = {0};
-        final float numberOfMission=10;
-        final ChildView childview;
-        childview= findViewById(R.id.childview_idd);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,46 +54,6 @@ public class MainChildActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
 
-        //button mission
-        final Boolean[] buttonFinishClicked = {false};
-        final Button buttonFinish= findViewById(R.id.button_fin);
-        if(childview==null)
-            Log.d("button null null", "click");
-        buttonFinish.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                Log.d("button", "click");
-                buttonFinishClicked[0] =!buttonFinishClicked[0];
-                if(buttonFinishClicked[0])
-                {
-                    Log.d("button in in", "click");
-                    buttonFinish.setBackgroundColor(Color.rgb(151, 68, 68));
-                    if(childview!=null) {
-                        if(angle[0] <360) {
-                            angle[0] += (360 / numberOfMission);
-                        }
-                        else {
-                            angle[0] =360 / numberOfMission;
-                        }
-
-                        childview.angleIncrease(angle[0]);
-                        Log.d("button in", "click");
-                        childview.invalidate();
-                    }
-                    else{
-                        Log.d("button null", "click");
-                    }
-
-                }
-                else
-                {
-                    buttonFinish.setBackgroundColor(Color.rgb(181, 131, 141));
-                }
-                buttonFinishClicked[0]=!buttonFinishClicked[0];
-            }
-
-        });
-
         //button logout
         final Button button_logout =  findViewById(R.id.button_logout);
         button_logout.setOnClickListener(new View.OnClickListener() {
@@ -104,22 +61,21 @@ public class MainChildActivity extends AppCompatActivity {
             public void onClick(View v) {
                 button_logout.setBackgroundColor(Color.rgb(204, 233, 232));
                 Log.d("button click", "yes");
-                Toast.makeText(MainChildActivity.this, "log out!", Toast.LENGTH_SHORT).show();
-                Intent LogoutActivityIntent = new Intent(MainChildActivity.this, MainActivity.class);
+                Toast.makeText(WishActivity.this, "log out!", Toast.LENGTH_SHORT).show();
+                Intent LogoutActivityIntent = new Intent(WishActivity.this, MainActivity.class);
                 startActivity(LogoutActivityIntent);
             }
         });
 
-        //button my wish
-        final Button button_wish= findViewById(R.id.button_my_wish);
-        button_wish.setOnClickListener(new View.OnClickListener() {
+        //button coin
+        final Button button_coin = findViewById(R.id.button_coin);
+        button_coin.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                button_wish.setBackgroundColor(Color.rgb(204, 233, 232));
-                Intent LogoutActivityIntent = new Intent(MainChildActivity.this, WishActivity.class);
-                startActivity(LogoutActivityIntent);
+            public void onClick(View v){
+                
             }
         });
+
 
     }
 
