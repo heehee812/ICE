51a96c5ba506ce31142b1e27e27c1f31cbbb1ff0
恩宇 <e.enyuu812@gmail.com>
2020-06-18 07:19:35 +0800

    parent bar

--- app/src/main/java/com/example/ice/MainParentActivity.java
+++ app/src/main/java/com/example/ice/MainParentActivity.java
@@ -13,6 +13,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.ice.Views.ParentView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -37,12 +38,73 @@ public class MainParentActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final float[] angle = {0};
+        final float numberOfMission=30;
+        final ParentView parentview;
+        parentview= findViewById(R.id.parentview_idd);
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setDrawerLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
 
+        //button mission
+        final Boolean[] buttonFinishClicked = {false};
+        final Button buttonCheck= findViewById(R.id.button_gift_check);
+        final Button buttonWait= findViewById(R.id.button_gift_wait);
+        final Button buttonFinish= findViewById(R.id.button_fin);
+        final int[] GiveGift = {0};
+        if(parentview==null)
+            Log.d("button null null", "click");
+        buttonFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Log.d("button", "click");
+                buttonFinishClicked[0] =!buttonFinishClicked[0];
+                if(buttonFinishClicked[0])
+                {
+                    Log.d("button in in", "click");
+                    Toast.makeText(MainParentActivity.this, "mission success !", Toast.LENGTH_SHORT).show();
+                    if(parentview!=null) {
+                        if(angle[0] <360) {
+                            angle[0] += (360 / numberOfMission);
+                            GiveGift[0] = 0;
+                            if(angle[0]==360) {
+                                GiveGift[0] = 1;
+                                buttonFinish.setVisibility(View.INVISIBLE);
+                                buttonCheck.setVisibility(View.VISIBLE);
+                                buttonWait.setVisibility(View.VISIBLE);
+                            }
+                            else
+                                GiveGift[0] =0;
+                        }
+                        else {
+                            GiveGift[0] =0;
+                            angle[0] =360 / numberOfMission;
+                        }
+                        parentview.giveGiftOrNot(GiveGift[0]);
+                        parentview.angleIncrease(angle[0]);
+                        Log.d("button in", "click");
+                        parentview.invalidate();
+                    }
+                    else{
+                        Log.d("button null", "click");
+                    }
 
+                }
+                else
+                {
+                    buttonFinish.setBackgroundColor(Color.rgb(181, 131, 141));
+                }
+                buttonFinishClicked[0]=!buttonFinishClicked[0];
+            }
+
+        });
 
         //button logout
         final Button button_logout =  findViewById(R.id.button_logout);
@@ -67,13 +129,43 @@ public class MainParentActivity extends AppCompatActivity {
                 startActivity(LogoutActivityIntent);
             }
         });
-        
+
+        //button my mission
+        final Button button_mission= findViewById(R.id.button_mission);
+        button_mission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button_mission.setBackgroundColor(Color.rgb(204, 233, 232));
+                Intent LogoutActivityIntent = new Intent(MainParentActivity.this, MissionActivity.class);
+                startActivity(LogoutActivityIntent);
+            }
+        });
+
+
+        //button circle on monster
+        final Boolean[] circleClicked = {false};
+        final Button button_circle= findViewById(R.id.button_circle_on_monster);
+        final TextView text_rule= findViewById(R.id.text_rule_eat);
+        button_circle.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(!circleClicked[0]) {
+                    Log.d("click circle", "click");
+                    text_rule.setVisibility(View.VISIBLE);
+                    circleClicked[0] = !circleClicked[0];
+                }
+                else{
+                    text_rule.setVisibility(View.INVISIBLE);
+                    circleClicked[0] = !circleClicked[0];
+                }
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_child, menu);
+        getMenuInflater().inflate(R.menu.main_parent, menu);
         return true;
     }
 
--- app/src/main/java/com/example/ice/Views/ParentView.java
+++ app/src/main/java/com/example/ice/Views/ParentView.java
@@ -1,4 +1,115 @@
 package com.example.ice.Views;
 
-public class ParentView {
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+
+import com.example.ice.R;
+
+public class ParentView extends View{
+
+    private float angle=0;
+    private int GiveGift= 0;
+    private Paint paint= new Paint();
+
+    public ParentView(Context context){
+        super(context);
+
+        init(null);
+    }
+
+    public ParentView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+
+        init(attrs);
+    }
+
+    public ParentView(Context context, AttributeSet attrs, int defStyleAttrs){
+        super(context, attrs, defStyleAttrs);
+
+        init(attrs);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public ParentView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes){
+        super(context, attrs, defStyleAttr, defStyleRes);
+
+        init(attrs);
+    }
+
+    private void init(@Nullable AttributeSet set){
+
+    }
+
+    //angle
+    public void angleIncrease(float angle)
+    {
+        Log.d("angleIncrease", "found");
+        this.angle=angle;
+    }
+
+    //gift
+    public void giveGiftOrNot(int GiveGift)
+    {
+        this.GiveGift=GiveGift;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @SuppressLint("DrawAllocation")
+    @Override
+    protected void onDraw(Canvas canvas){
+
+        int x=getWidth()/2, y=3*getHeight()/5;
+        int imgXmonster=800, imgYmonster=1000;
+        int imgXgift= 1500, imgYgift= 1800;
+        int radius= 200;
+
+        //background
+        @SuppressLint("DrawAllocation") Bitmap child_background= BitmapFactory.decodeResource(getResources(), R.drawable.child_background);
+        child_background = Bitmap.createScaledBitmap(child_background, getWidth(), getHeight(), true);
+        canvas.drawBitmap(child_background, 0 , 0, paint);
+
+        //monster
+        @SuppressLint("DrawAllocation") Bitmap monster2= BitmapFactory.decodeResource(getResources(), R.drawable.monster2);
+        monster2 = Bitmap.createScaledBitmap(monster2, imgXmonster, imgYmonster, true);
+        if(GiveGift==0)
+            canvas.drawBitmap(monster2, x-imgXmonster/2 , y-imgYmonster/2, paint);
+
+        //arc
+        Log.d("angle", String.valueOf(angle));
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.rgb(225, 225, 195));
+        RectF oval = new RectF( x-radius, y, x+radius, y+2*radius);
+        if(GiveGift==0)
+            canvas.drawArc(oval,270, angle, true, paint);
+
+        //arc2
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(6);
+        paint.setColor(Color.rgb(105, 118, 155));
+        if(GiveGift==0)
+            canvas.drawArc(oval,270, angle, true, paint);
+
+
+        //circle2
+        paint.setColor(Color.rgb(105, 118, 155));
+        paint.setStrokeWidth(6);
+        paint.setStyle(Paint.Style.STROKE);
+        if(GiveGift==0)
+            canvas.drawCircle(x, y+radius, radius, paint);
+
+    }
 }
